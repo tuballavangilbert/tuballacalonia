de5d96c4017358a6386f136c27c3832e5f2a245b
tuballavangilbert <60138667+tuballavangilbert@users.noreply.github.com>
2021-01-10 16:41:01 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: MyApp(),
+  ));
+}
 
 var budget = 0.00;
+var savings = 0.00;
 String strbudget = budget.toStringAsFixed(2);
+String strSavings = savings.toStringAsFixed(2);
 
 class MyApp extends StatelessWidget {
   @override
@@ -32,7 +40,7 @@ class MyApp extends StatelessWidget {
               ),
               child: Row(
                 children: <Widget>[
-                  Text(strbudget, style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),),
+                  Text(strbudget, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),),
                   new Spacer(),
                   IconButton(
                     icon: Icon(Icons.arrow_forward_ios),
@@ -46,12 +54,6 @@ class MyApp extends StatelessWidget {
                 ]
               )
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-              ),
-              child: Text('Edit', style: TextStyle(fontSize: 10),),
-            )
           ],
         ),
       ),
@@ -69,7 +71,150 @@ class BudgetMenu extends StatelessWidget {
         appBar: AppBar(
           title: Text('Cartshier'),
           backgroundColor: Colors.black,
-        )));
+          leading: IconButton(icon: Icon(Icons.arrow_back_ios),
+                              color: Colors.white,
+                              onPressed:(){
+                                Navigator.pop(context);
+                              }
+                             )
+        ),
+        body:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            FlatButton(
+              onPressed:(){
+                Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => EditBudget(),
+                      ));
+              },
+    color: Colors.deepOrange,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18.0),
+      side: BorderSide(color: Colors.black),
+    ),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+        Icon(Icons.edit, color: Colors.black),
+        Text(
+            "EDIT BUDGET",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+            color: Colors.black, fontSize: 18.0),
+        ),
+    ],
+),
+          ),
+             FlatButton(
+              onPressed:(){
+                Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Savings(),
+                      ));
+              },
+    color: Colors.deepOrange,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18.0),
+      side: BorderSide(color: Colors.black),
+    ),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+        Icon(Icons.control_point_rounded, color: Colors.black),
+        Text(
+            "ADD TO SAVINGS",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+            color: Colors.black, fontSize: 18.0),
+        ),
+    ],
+),
+          ),
+          ]
+        )
+      )
+    );
 }}
-        
-        
+
+class EditBudget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          title: Text('Cartshier'),
+          backgroundColor: Colors.black,
+          leading: IconButton(icon: Icon(Icons.arrow_back_ios),
+                              color: Colors.white,
+                              onPressed:(){
+                                Navigator.pop(context);
+                              }
+                             )
+        ),
+      body: new Container(
+          padding: const EdgeInsets.all(40.0),
+          child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new TextField(
+            decoration: new InputDecoration(labelText: "Enter your budget"),
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+    FilteringTextInputFormatter.digitsOnly
+], // Only numbers can be entered
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class Savings extends StatelessWidget {
+  @override
+   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Cartshier'),
+          backgroundColor: Colors.black,
+          leading: IconButton(icon: Icon(Icons.arrow_back_ios),
+                              color: Colors.white,
+                              onPressed:(){
+                                Navigator.pop(context);
+                              }
+                             )
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Row(
+                children:<Widget>[
+                  Icon(Icons.check, color:Colors.greenAccent),
+                  Text("Successfully added to savings.", style: TextStyle(color: Colors.greenAccent))
+                ]
+              )
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children:<Widget>[
+                  Text("NEW SAVINGS BALANCE", textAlign: TextAlign.left),
+                  Text(strSavings, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold), textAlign: TextAlign.left)
+                ]
+              )
+            )
+          ]
+        )));
+
+}}
